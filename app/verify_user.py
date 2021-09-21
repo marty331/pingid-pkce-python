1d40d17b7e4baa15e38484f5e604c52100489cbf
Rohit Mishra <rohit.kumar2@shell.com>
2021-09-21 15:34:09 +0530

    Update verify_user.py

--- app/verify_user.py
+++ app/verify_user.py
@@ -14,12 +14,44 @@ def login_required(f):
 
         try:
             token = authorization.split(' ')[1]
-            resp = decode(token, None, verify=False, algorithms=['HS256'])
-            g.user = resp['mail']
+            #CLIENT_ID, ISSUER --> read from env variable
+            url = ISSUER +"/as/token.oauth2"
+            data = {"token": authorization, "grant_type": "urn:pingidentity.com:oauth2:grant_type:validate_bearer", "client_id": CLIENT_ID}
+            headers = {'Content-Type': "application/x-www-form-urlencoded"}
+            response = requests.post(url, data=data, headers=headers)
+            response_Json = response.json()
+                
+            #resp = decode(token, None, verify=False, algorithms=['HS256'])
+            #g.user = resp['mail']
             # code here to validate if user should be part of DAP group 
+            if 'error' not in response_Json:
+                    g.user = response_Json['access_token']['mail']
+                else:
+                    return json.dumps({'error': 'invalid authorization token/expired token'}), 403, {'Content-type': 'application/json'}
+                
         except exceptions.DecodeError:
             return json.dumps({'error': 'invalid authorization token'}), 403, {'Content-type': 'application/json'}
 
         return f(*args, **kwargs)
 
     return wrap
+
+def role_access(allowed_roles):
+    def decorator(f):
+        @wraps(f)
+        def wrapper():
+            sqlmanager = SQLManager.get_instance()
+            try:
+                user = g.user
+                role_id = sqlmanager.get_data(Users, email = user).value(Users.role_id)
+                role = sqlmanager.get_data(
+                    Roles, role_id=role_id).value(Roles.role)
+                if role not in allowed_roles:
+                    return json.dumps({'error': 'not authorized to access'}), 401, {'Content-type': 'application/json'}
+            except Exception as ex:
+                logger.error("Exception occured at role access: {}".format(ex))
+                return json.dumps({"error": "Error occured at role access level"})
+
+            return f()
+        return wrapper
+    return decorator
